185ad0079fa7efb903becb49bf86663421e3d637
logonrm <logonrm@fiap.com.br>
2017-08-21 20:52:15 -0300

    Adicionado ExemploMvc02

---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Controllers/VagaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Fiap.Exemplo02.MVC.Models;
+
+namespace Fiap.Exemplo02.MVC.Controllers
+{
+    public class VagaController : Controller
+    {
+
+        private static List<Vaga> _banco = new List<Vaga>();
+        
+        // GET: Vaga
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            PopulaComboNiveis();
+            return View();
+        }
+
+        private void PopulaComboNiveis()
+        {
+            List<string> niveis = new List<string>();
+            niveis.Add("Junior");
+            niveis.Add("Pleno");
+            niveis.Add("Sênior");
+            ViewBag.options = new SelectList(niveis);
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Vaga vaga)
+        {
+            PopulaComboNiveis();
+            _banco.Add(vaga);
+            ViewBag.msg = "Cadastrado com Sucesso";
+            return View();
+        }
+
+        public ActionResult Listar()
+        {
+            return View(_banco);
+        }
+    }
+}
---
+++ Fiap.Exemplo02.MVC/Fiap.Exemplo02.MVC/Models/Vaga.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Fiap.Exemplo02.MVC.Models
+{
+    public class Vaga
+    {
+        public string Cargo { get; set; }
+        [Display(Name = "Descrição")]
+        public string Descricao { get; set; }
+        [Display(Name = "Salário")]
+        public decimal Salario { get; set; }
+        [Display(Name = "Vale Refeição")]
+        public bool Vr { get; set; }
+        public string Nivel { get;set; }
+    }
+}
