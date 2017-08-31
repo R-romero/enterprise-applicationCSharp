fb017bfcf603cbbc754d00c74d4abc47667c7f2b
RODRIGO OLIVEIRA ROMERO <RM76781@fiap.com.br>
2017-08-30 22:27:49 -0300

    Projeto Sacolão Finalizado

---
+++ Fiap.Exemplo03.MVC/Fiap.Exemplo03.MVC/Controllers/FrutaController.cs
@@ -0,0 +1,38 @@
+﻿using Fiap.Exemplo03.MVC.Models;
+using Fiap.Exemplo03.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo03.MVC.Controllers
+{
+    public class FrutaController : Controller
+    {
+        private SacolaoContext _context = new SacolaoContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Cadastrar(Fruta fruta)
+        {
+            fruta.DataCadastro = DateTime.Now;
+            _context.Frutas.Add(fruta);
+            _context.SaveChanges();
+            TempData["msg"] = "Cadastrado!";
+            //Para não cadastrar novamente no F5
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_context.Frutas.ToList());
+        }
+    }
+}
---
+++ Fiap.Exemplo03.MVC/Fiap.Exemplo03.MVC/Controllers/ProdutorController.cs
@@ -0,0 +1,37 @@
+﻿using Fiap.Exemplo03.MVC.Models;
+using Fiap.Exemplo03.MVC.Persistencia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fiap.Exemplo03.MVC.Controllers
+{
+    public class ProdutorController : Controller
+    {
+
+        private SacolaoContext _context = new SacolaoContext();
+
+        [HttpGet]
+        public ActionResult Cadastrar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cadastrar(Produtor produtor)
+        {
+            produtor.DataCadastro = DateTime.Now;
+            _context.Produtor.Add(produtor);
+            _context.SaveChanges();
+            TempData["msg"] = "Produtor Cadastrado!";
+            return RedirectToAction("Cadastrar");
+        }
+
+        [HttpGet]
+        public ActionResult Listar()
+        {
+            return View(_context.Produtor.ToList());
+        }
+    }
+}
